0a7e0ec19f02017e54d0eed219a4abb9f033bf9d
cmilne78 <79056900+cmilne78@users.noreply.github.com>
2022-04-19 16:42:14 -0400

    Commit me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,48 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import styled from "styled-components"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
+  <Wrapper>
+    <Logo src="/images/logos/react-logo.svg" alt="logo" />
+    <Title>Build a web app</Title>
+    <Caption>20 sections!</Caption>
+    <Description>Learn how we build.</Description>
+  </Wrapper>
 )
 
 export default IndexPage
+
+const Wrapper = styled.div`
+  height: 1247px;
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+`
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`
+const Title = styled.h1`
+  max-width: 500px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 60px;
+  line-height: 72px;
+  color: #ffffff;
+  mix-blend-mode: normal;
+  text-shadow: 0px 20px 40px rgba(0, 0, 0, 0.3);
+`
+
+const Caption = styled.p`
+  font-style: normal;
+  font-size: 15px;
+  line-height: 130%;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.7);
+`
+
+const Description = styled.p`
+  max-width: 400px;
+  font-style: normal;
+  font-size: 20px;
+  line-height: 140%;
+  color: #ffffff;
+`
